636cbf98d5e147a14c86f9b243febfcddb450dc1
barmie9 <bartek.mierzwinski.99@gmail.com>
2022-03-17 15:02:25 +0100

    Fixed replay mode in LearningActivity

--- app/src/main/java/com/example/angielski/LearningActivity.kt
+++ app/src/main/java/com/example/angielski/LearningActivity.kt
@@ -7,10 +7,9 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.BaseColumns
-import android.util.Log
 import android.view.View
 import com.example.angielski.databinding.ActivityLearningBinding
-import com.example.angielski.databinding.ActivityMainBinding
+//import com.example.angielski.databinding.ActivityMainBinding
 import java.util.*
 
 class LearningActivity : AppCompatActivity() {
@@ -24,14 +23,16 @@ class LearningActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learning)
 
-        val TAG = "TAG"
+        // ----- Tag for the tests -----
+//        val TAG = "TAG"
 
-        //2 linijki ktore trzeba dodac https://youtu.be/qbq5PR-l5ZU?t=634
+        // ----- How to get to id layout -----
         binding = ActivityLearningBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // ----- Take data from the main activity, REPEAT OR LEARNING -----
-        val isLearning = getIntent().getBooleanExtra("is_learning",false)
+        val isLearning = intent.getBooleanExtra("is_learning",false)
+//        val isLearning = getIntent().getBooleanExtra("is_learning",false)
 
         // ----- Get local file (variable) "Settings", to read an edit "editor"-----
         val sharedPref: SharedPreferences = getSharedPreferences("Settings", MODE_PRIVATE)
@@ -45,7 +46,7 @@ class LearningActivity : AppCompatActivity() {
         val query: Array<String> = arrayOf("","")
 
         // ----- Create a few query to db, to repeating or learning  -----
-        if(isLearning){
+        if(isLearning){ //learning
             query1 = "SELECT * FROM " + TableInfo.TABLE_NAME +
                     " WHERE ${TableInfo.TABLE_COLUMN_LEARNED_PL_TO_ANG} = 'f' "
             query2 = "SELECT * FROM " + TableInfo.TABLE_NAME +
@@ -53,7 +54,7 @@ class LearningActivity : AppCompatActivity() {
             query[0] = query1
             query[1] = query2
         }
-        else{
+        else{ //repeating
             query1 = "SELECT * FROM " + TableInfo.TABLE_NAME +
                     " WHERE ${TableInfo.TABLE_COLUMN_LEARNED_PL_TO_ANG} = 't' "+
                     " AND DATETIME('now', 'localtime') >= DATETIME(${TableInfo.TABLE_COLUMN_DATE},${TableInfo.TABLE_COLUMN_REPLAY_PL_TO_ANG}); "
@@ -66,35 +67,38 @@ class LearningActivity : AppCompatActivity() {
 
         }
 
+
         // ----- get two tables from db for the ang->pl and pl->ang -----
         val result = Array(2){i -> db.rawQuery(query[i],null)}
-//        val result1 = db.rawQuery(query1,null)
-//        val result2 = db.rawQuery(query2,null)
+
 
         // ----- create a few variables for proper operation -----
-        var englishWord: String = ""
-        var polishWord: String = ""
-        var answere: String = ""
+        var englishWord = ""
+        var polishWord = ""
+        var answere = ""
         var wordsCounter = arrayOf(0,0) // Counter of correct  words
         val wordsToLearnName = arrayOf("WordsToLearnPlToEng","WordsToLearnEngToPl")
         val wordsToLearn  = arrayOf(sharedPref.getInt("WordsToLearnPlToEng",9),sharedPref.getInt("WordsToLearnEngToPl",9)) // number of words to learn, for index 0 - pl->ang and 1 ang->pl
-        var ii:Int =0 // Actual words (Column) - Words
-        var jj:Int =0 // ang-> pl or pl-> ang
+        var ii =0 // Actual words (Column) - Words
+        var jj =0 // ang-> pl or pl-> ang
 
-        // ----- For the tests ----
-        Log.d(TAG, "${wordsToLearn[0]} , ${wordsToLearn[1]}")
 
         // ----- Create and fill a queue. Needed to repeat words until they are learned -----
         var myQueue: Queue<Array<Int>> = LinkedList<Array<Int>>()
-        var exampleTab: Array<Int> = arrayOf(0,1)
-        for(i in 0..1)
-            for(j in 0 until wordsToLearn[i])
-                myQueue.add(arrayOf(i,j))
+//        var exampleTab: Array<Int> = arrayOf(0,1)
+        if(isLearning){
+            for(i in 0..1)
+                for(j in 0 until wordsToLearn[i])
+                    myQueue.add(arrayOf(i,j))
+        }
+        // -----
+        else{
+            for(i in 0..1)
+                for(j in 0 until result[i].count)
+                    myQueue.add(arrayOf(i,j))
+        }
 
 
-
-
-//        Log.d(TAG,"wordsCounter->${wordsCounter[jj]} , wordsCounter->${wordsToLearn[jj]} , ii->${ii} , jj->${jj} ,")
 
         // ----- if queue is not empty fill the initial layout elements -----
         if(!myQueue.isEmpty()){
@@ -151,7 +155,7 @@ class LearningActivity : AppCompatActivity() {
                 editor.putInt(wordsToLearnName[jj], wordsToLearn[jj] - wordsCounter[jj])
                 editor.apply()
 
-                // ----- Good sound -----
+                // ----- Positive sound effect -----
                 MediaPlayer.create(this,R.raw.duolingo_1).start()
                 binding.imageViewAnswere.setImageResource(R.drawable.answere_1)
 
@@ -161,9 +165,10 @@ class LearningActivity : AppCompatActivity() {
                     result[jj].getString(result[jj].getColumnIndex(TableInfo.TABLE_COLUMN_REPLAY_PL_TO_ANG)))
                 val tableColumnIsLearned = arrayOf("learned_pl_to_ang","learned_ang_to_pl")
                 val tableColumnReplay = arrayOf("replay_time_pl_to_ang","replay_time_ang_to_pl")
-                var newReplay: String = ""
+                val newReplay: String
                 var newIsLearned = "'t'"
 
+                // ----- Change the time for the next replay. If all replays are done set column (in db) is learned "ct" -----
                 when (replay[jj]){
                     "+1 second" -> newReplay = "'+2 second'"
                     "+2 second" -> newReplay = "'+3 second'"
@@ -180,13 +185,13 @@ class LearningActivity : AppCompatActivity() {
                 }
 
 
+                // ----- Update db -----
                 val queryUpdate = "UPDATE ${TableInfo.TABLE_NAME} "+
                         "SET ${tableColumnIsLearned[jj]} = ${newIsLearned}, " +
                         "${TableInfo.TABLE_COLUMN_DATE} = DATETIME('now', 'localtime'), " +
                         "${tableColumnReplay[jj]} =  $newReplay "+
                         " WHERE ${BaseColumns._ID} = ${result[jj].getString(result[jj].getColumnIndex(BaseColumns._ID))};" //_id  ${result.getString(result.getColumnIndex(BaseColumns._ID))}
 
-//                db.rawQuery(query_update, null)
                 db.execSQL(queryUpdate)
 
 
@@ -203,23 +208,22 @@ class LearningActivity : AppCompatActivity() {
             binding.buttonContinue.visibility = View.VISIBLE
 
             hideKeyboard()
-            binding.editTextAnswere.setText("")
-
-
 
         }
 
         // ----- Support for the second "CONTINUE" button -----
         binding.buttonContinue.setOnClickListener {
+            // ----- Clear editTextAnswere -----
+            binding.editTextAnswere.setText("")
+
+            // ----- If queue is empty then Finish lesson -----
             if(myQueue.isEmpty()){
                 setViewFinishLesson()
             }
-            else{
+            else{ // Otherwise, load the data
                 val myTab = myQueue.poll()
                 ii = myTab[1]
                 jj = myTab[0]
-
-                Log.d(TAG,"wordsCounter->${wordsCounter[jj]} , wordsCounter->${wordsToLearn[jj]} , ii->${ii} , jj->${jj} ,")
 
                 binding.buttonContinue.visibility = View.INVISIBLE
                 binding.buttonCheckAnswere.visibility = View.VISIBLE
@@ -227,11 +231,15 @@ class LearningActivity : AppCompatActivity() {
                 if(result[jj].moveToPosition(ii)){
                     polishWord = result[jj].getString(result[jj].getColumnIndex(TableInfo.TABLE_COLUMN_POLISH))
                     englishWord = result[jj].getString(result[jj].getColumnIndex(TableInfo.TABLE_COLUMN_ENGLISH))
+
+                    // ----- swap values of variables if the translation eng->pl -----
                     if (jj == 1){
                         var buf = polishWord
                         polishWord = englishWord
                         englishWord = buf
                     }
+
+                    // ----- Set the textView  with polish or english word -----
                     binding.textViewWords.text = polishWord
 
                     val id = resources.getIdentifier(result[jj].getString(result[jj].getColumnIndex(TableInfo.TABLE_COLUMN_ID_PICTURE)), "drawable", packageName )
@@ -242,6 +250,7 @@ class LearningActivity : AppCompatActivity() {
 
         }
 
+        // ----- The sound of word -----
         binding.imageButtonSound.setOnClickListener{
             val id = resources.getIdentifier(result[jj].getString(result[jj].getColumnIndex(TableInfo.TABLE_COLUMN_ID_PICTURE)), "raw", packageName )
             MediaPlayer.create(this,id).start()
@@ -260,8 +269,10 @@ class LearningActivity : AppCompatActivity() {
 
         // ----- Support for the "End lessons" button that is displayed after the end of the lesson -----
         binding.buttonFinishLesson.setOnClickListener {
-            finish() // Kończy działanie bierzącej aktywności i powraca do głownego MENU
-            db.close() // DO SPRAWDZENIA !!!!!!!!!!!!!!
+            finish() // The end of this activity
+            result[0].close() // Close the cursor
+            result[1].close() // Close the cursor
+            db.close() // Close DataBase
         }
 
 
@@ -282,6 +293,7 @@ class LearningActivity : AppCompatActivity() {
         binding.buttonCheckAnswere.visibility = View.INVISIBLE
         binding.editTextAnswere.visibility = View.INVISIBLE
         binding.imageButtonSound.visibility = View.INVISIBLE
+        binding.imageViewAnswere.visibility = View.INVISIBLE
         binding.imageViewWord.setImageResource(R.drawable.finish_lesson)
         binding.textViewWords.text = "KONIEC LEKCJI"
         binding.textViewWords.textSize = 40f
